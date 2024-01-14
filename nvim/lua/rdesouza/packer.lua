47fddf64d22b411b80b3234697b88d14b75c5997
Ramiro de Souza <ramirods93@gmail.com>
2024-01-14 17:17:13 -0300

    nvim changes

--- nvim/lua/rdesouza/packer.lua
+++ nvim/lua/rdesouza/packer.lua
@@ -2,7 +2,7 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
-	use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', requires = {{ 'nvim-lua/plenary.nvim' }} }
+	use { 'nvim-telescope/telescope.nvim', tag = '0.1.x', requires = {{ 'nvim-lua/plenary.nvim' }} }
 	use { 'catppuccin/nvim', as = 'catppuccin' }
 	use { 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}}
 	use { 'nvim-treesitter/playground' }
@@ -31,4 +31,9 @@ return require('packer').startup(function(use)
 			{'rafamadriz/friendly-snippets'}
 		}
 	}
+    use {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    }
 end)
